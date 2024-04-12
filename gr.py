746e1f30e88f02156ec231981d6299da83c6616f
HuberV1 <huber.londonov@udea.edu.co>
2024-04-12 10:51:35 -0500

    punto 3, visualizar, forma, tamaño

--- gr.py
+++ gr.py
@@ -1,3 +1,4 @@
+import pandas as np
 #punto1
 import numpy as np
 matriz_4d = np.random.rand(30, 20, 10, 2000)  
@@ -6,3 +7,7 @@ print(matriz_4d.size)
 #punto#2
 matriz_3d = matriz_4d.copy().squeeze()  
 
+#punto3
+print("Dimensiones:", matriz_3d.ndim)
+print("Forma:", matriz_3d.shape)
+print("Tamaño:", matriz_3d.size)
