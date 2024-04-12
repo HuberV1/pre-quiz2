50287f1a7e15a1d7683b63c4f0c9ec8817b49385
HuberV1 <huber.londonov@udea.edu.co>
2024-04-12 10:45:29 -0500

    se crea la copia de la matriz, y se reduce una dimencion, con el metodo squeeze, y se corrige la D del P1

--- gr.py
+++ gr.py
@@ -1,3 +1,8 @@
+#punto1
 import numpy as np
-matriz_4d = np.random.rand(30, 40, 50, 10)  
+matriz_4d = np.random.rand(30, 20, 10, 2000)  
 print(matriz_4d.size)
+
+#punto#2
+matriz_3d = matriz_4d.copy().squeeze()  
+
